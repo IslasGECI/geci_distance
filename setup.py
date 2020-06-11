9e63d87aa83f155b55b302279452aa746a836fd6
Fernando <fernando.alvarez@islas.org.mx>
2020-06-11 12:24:10 -0700

    agrega setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="GECI_Distance",
+    version="0.1.0",
+    packages=["geci_distance"],
+    author="Fernando Alvarez",
+    python_requires='>=3.6'
+)
